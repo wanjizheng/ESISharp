c2e069aa7b253c0ed0261e2a153288de80a58e9f
W Anders <wr_anders@yahoo.com>
2017-11-03 10:38:57 -0500

    Started Scopes
    Set ESISharp.Test dependency on ESISharp
    Added Scope class
    Added Alliance Scope class

--- ESISharp.Test/UnitTest.cs
+++ ESISharp.Test/UnitTest.cs
@@ -1,10 +1,12 @@
+using ESISharp.Model.Enumeration.Scopes;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ESISharp.Test
 {
     public class UnitTests
     {
-        
+
     }
 }
---
+++ ESISharp/Model/Enumeration/Scopes/Scope.cs
@@ -0,0 +1,23 @@
+﻿using ESISharp.Model.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESISharp.Model.Enumeration.Scopes
+{
+    public sealed partial class Scope : FakeEnumerator
+    {
+        internal static readonly Dictionary<string, Scope> Lookup = new Dictionary<string, Scope>();
+
+        internal Scope(string value) : base(value)
+        {
+            if (value != string.Empty)
+            {
+                Lookup.Add(value, this);
+            }
+        }
+
+        public static readonly IEnumerable<Scope> All = Lookup.Values.ToList();
+
+        public static readonly Scope None = new Scope(string.Empty);
+    }
+}
