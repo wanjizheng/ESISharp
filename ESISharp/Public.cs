7037eff408a5afc1a5d49bbecd6271d97e3ab08d
W Anders <wr_anders@yahoo.com>
2017-11-03 12:43:28 -0500

    Testing CI tools

--- ESISharp/Public.cs
+++ ESISharp/Public.cs
@@ -1,4 +1,6 @@
 ﻿using ESISharp.Model.Abstract;
+using System.IO;
+using System.Text;
 
 namespace ESISharp
 {
@@ -7,6 +9,23 @@ namespace ESISharp
         public Public() : base()
         {
 
+        }
+    }
+
+    public class TestingCodeSmellDetection
+    {
+        private FileStream fs;
+
+        public void OpenResource(string path)
+        {
+            this.fs = new FileStream(path, FileMode.Open);
+        }
+
+        public void WriteToFile(string path, string text)
+        {
+            var fs = new FileStream(path, FileMode.Open);
+            var bytes = Encoding.UTF8.GetBytes(text);
+            fs.Write(bytes, 0, bytes.Length);
         }
     }
 }
